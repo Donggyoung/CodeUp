5eb4224979903c51575a2e9ab21871e8f324588a
Donggyoung <43374968+Donggyoung@users.noreply.github.com>
2019-07-25 18:34:56 +0900

    Update 1406.c

--- 1406.c
+++ 1406.c
@@ -3,7 +3,7 @@
 int main() {
 	char a[10];
 	gets(a);
-	if (strcmp(a,"love") == 0) { // strcmp(a,b)의 리턴 값이 0이면 
+	if (strcmp(a,"love") == 0) { // strcmp(a,b)의 리턴 값이 0이면 a,b는 같다.
 		printf("I love you.");
 	}
 	
